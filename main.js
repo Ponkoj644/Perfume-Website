d711a9b0873257f56bc0107c226332ac3ad81460
Ponkoj Mondol <ponkojmonndal2014@gmail.com>
2024-08-16 17:34:02 +0600

    scroll up button added

--- main.js
+++ main.js
@@ -14,3 +14,22 @@ navLink.forEach((link) => {
     hamburger.classList.toggle("ri-close-large-line");
   });
 });
+
+// ~~~~~~~~~~~~~~~~~~ SHOW SCROLL UP BUTTONS ~~~~~~~~~~~~~~~~//
+const scrollUp = () => {
+  const scrollUpBtn = document.getElementById("scroll-up");
+
+  if (this.scrollY >= 250) {
+    scrollUpBtn.classList.remove("-bottom-1/2");
+    scrollUpBtn.classList.add("bottom-10");
+  } else {
+    scrollUpBtn.classList.add("-bottom-1/2");
+    scrollUpBtn.classList.remove("bottom-10");
+  }
+};
+
+window.addEventListener("scroll", scrollUp);
+
+// ~~~~~~~~~~~~~~~~~~ ACTIVE LINK ~~~~~~~~~~~~~~~~//
+
+
