ac08e9fbdb4436da2ed088d2c720e959a4974ffe
Ponkoj Mondol <ponkojmonndal2014@gmail.com>
2024-08-22 00:30:18 +0600

    Animation with scrollrevealjs

--- main.js
+++ main.js
@@ -30,6 +30,48 @@ const scrollUp = () => {
 
 window.addEventListener("scroll", scrollUp);
 
-// ~~~~~~~~~~~~~~~~~~ ACTIVE LINK ~~~~~~~~~~~~~~~~//
+// ~~~~~~~~~~~~~~~~~~ SCROLL SECTION ACTIVE LINK ~~~~~~~~~~~~~~~~//
+const activeLink = () => {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav-link");
 
+  let current = "hero";
 
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+
+    if (this.scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((item) => {
+    item.classList.remove("active");
+
+    if (item.href.includes(current)) {
+      item.classList.add("active");
+    }
+  });
+};
+
+window.addEventListener("scroll", activeLink);
+
+// ~~~~~~~~~~~~~~~~~~ SCROLL SECTION ACTIVE LINK ~~~~~~~~~~~~~~~~//
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 200,
+  reset: true,
+});
+
+sr.reveal(
+  `.hero_text, .collection_heading, .popular_headding, .about_heading, .footer_content`
+);
+sr.reveal(`.hero_image`, { origin: "bottom", delay: 500 });
+sr.reveal(`.hero_content , .popular_card_3`, { origin: "bottom", delay: 100 });
+sr.reveal(`.collection_card , .banner_content`, { interval: 100 });
+
+sr.reveal(`.popular_card_1, .about_image`, { origin: "left" });
+sr.reveal(`.popular_card_2, .about_content`, { origin: "right" });
